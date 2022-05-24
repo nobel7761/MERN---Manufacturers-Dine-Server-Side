9bda96366c31e42551a2a0389f34991189b002e9
habiburnobel <habiburnobel@gmail.com>
2022-05-24 18:34:37 +0600

    code modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,36 +9,42 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("manufacturers");
-})
+app.get("/", (req, res) => {
+  res.send("manufacturers");
+});
 
 app.listen(port, () => {
-    console.log("Server Running...");
-})
-
-
-
-
-
+  console.log("Server Running...");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.65d9i.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 async function run() {
-    try {
-        await client.connect();
-        const toolsCollection = client.db('manufacturers-dine').collection('tools');
+  try {
+    await client.connect();
+    const toolsCollection = client.db("manufacturers-dine").collection("tools");
 
-        app.get('/tools', async (req, res) => {
-            const query = {};
-            const cursor = toolsCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
-        })
-    }
-    finally {
+    //getting all the tools
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
 
-    }
+    //getting tool by id
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await toolsCollection.findOne(query);
+      res.send(product);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
