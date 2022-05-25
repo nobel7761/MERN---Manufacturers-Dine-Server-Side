d73958197964aa6f81e56d48236bd97d3a9f9fe3
habiburnobel <habiburnobel@gmail.com>
2022-05-25 13:24:51 +0600

    create users api implemented

--- index.js
+++ index.js
@@ -27,8 +27,13 @@ async function run() {
   try {
     await client.connect();
     const toolsCollection = client.db("manufacturers-dine").collection("tools");
-    const ordersCollection = client.db("manufacturers-dine").collection("orders");
-    const reviewsCollection = client.db("manufacturers-dine").collection("reviews");
+    const ordersCollection = client
+      .db("manufacturers-dine")
+      .collection("orders");
+    const reviewsCollection = client
+      .db("manufacturers-dine")
+      .collection("reviews");
+    const userCollection = client.db("manufacturers-dine").collection("users");
 
     //getting all the tools
     app.get("/tools", async (req, res) => {
@@ -47,23 +52,56 @@ async function run() {
     });
 
     //placing an order
-    app.post('/order-placing', async (req, res) => {
+    app.post("/order-placing", async (req, res) => {
       const newOrder = req.body;
       const result = await ordersCollection.insertOne(newOrder);
       res.send(result);
-    })
+    });
 
     //getting all reviews
-    app.get('/reviews', async (req, res) => {
+    app.get("/reviews", async (req, res) => {
       const query = {};
       const cursor = reviewsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
 
-    })
+    //create users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
+    //updating user profile
+    app.put("/userprofile/:email", async (req, res) => {
+      const email = req.params.email;
+      const updateInfo = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          address: updateInfo.address,
+          education: updateInfo.education,
+          phone: updateInfo.phone,
+          linkedin: updateInfo.linkedin,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
 
-
+    //getting single user by the email!
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const product = await userCollection.findOne(query);
+      res.send(product);
+    });
   } finally {
   }
 }
