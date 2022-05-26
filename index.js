63275c2c61c14462b71d86e5ee728eb1f60104c3
nobel7761 <habiburnobel@gmail.com>
2022-05-26 20:19:39 +0600

    'changes'

--- index.js
+++ index.js
@@ -299,7 +299,7 @@ async function run() {
     })
 
     //payment
-    app.get('/pay/:id', verifyJWT, async (req, res) => {
+    app.get('/pay/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: ObjectId(id) };
       const result = await ordersCollection.findOne(query);
