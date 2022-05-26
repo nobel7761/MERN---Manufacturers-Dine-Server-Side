f55d11732a6cc4ec88b7a0fd39e35c04febdc093
nobel7761 <habiburnobel@gmail.com>
2022-05-26 18:36:28 +0600

    random

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ async function run() {
     });
 
     //getting single order using id
-    app.get("/order/:id", verifyJWT, async (req, res) => {
+    app.get("/order/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const order = await ordersCollection.findOne(query);
